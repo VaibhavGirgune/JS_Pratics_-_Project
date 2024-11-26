0af872eb9880fffa99855537c518576d4cb7b02b
vaibhav <vaibhavgirgune99@gmail.com>
2024-11-26 15:59:42 +0530

    loop files

---
+++ For_in.js
@@ -0,0 +1,11 @@
+ let obj ={
+    name: "vaibhav",
+    surname: "girgune",
+    Phon_no: 7058353049,
+    Addres: "Katraj Dhankwadi"
+ }
+
+ for ( let a in obj ){
+    console.log(a);
+ }
+
---
+++ For_of.js
@@ -0,0 +1,6 @@
+let v = ["a","c","v","b"]
+
+for (let n of v) {
+   console.log(n);
+
+}
